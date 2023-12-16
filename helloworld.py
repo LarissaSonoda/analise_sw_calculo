7d8f21e0f3f0c2bd89c01bbee91f2a6d3063a7ac
LarissaSonoda <sonoda.lari@gmail.com>
2023-12-15 22:08:06 -0300

    falas do luke no primeiro filme

--- helloworld.py
+++ helloworld.py
@@ -4,13 +4,26 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
+
+#SETANDO STOPWORDS
+
+stopwords = set(STOPWORDS)
+stopwords.update(["da", "meu", "em", "você", "de", "ao", "os", "que", "não",
+                  "sobre", "está", "para", "mas", "a", "o", "está",
+                  "ele", "eles", "ela", "elas", "eu", "quer", "é", "um", "vamos", "quem",
+                  "isso", "por", "muito", "aqui", "tem", "ok", "há", "estou", "aí", "fazer",
+                  "nos", "nosso", "nossa", "nós", "se", "e", "vai", "como", "quando", "o que", "quem", "quando",
+                  "esse", "este", "esses", "essa", "esta", "essas", "estas", "estes",
+                  "agora", "lá", "vou", "vai", "dá", "boa", "maior", "grupo",
+                  "teve", "tendo"])
+
+
 df = pd.read_excel("C:\\Users\\laris\\Desktop\\estudos_ferias20232\\python\\SW - Episode IV.xlsx", sheet_name = "SW_EpisodeIV_ptBR");
 
 summary = df.dropna(subset=['personagem'], axis=0)['personagem']
 
 all_summary = "\n".join(s for s in summary)
-stopwords = set(STOPWORDS)
-stopwords.update(["da", "meu", "em", "você", "de", "ao", "os"])
+
 wordcloud = WordCloud(stopwords=stopwords, background_color = "black", width=1600, height=800).generate(all_summary)
 # mostrar a imagem final
 fig, ax = plt.subplots(figsize=(10,6))
@@ -19,28 +32,71 @@ ax.set_axis_off()
 plt.imshow(wordcloud);
 wordcloud.to_file("personagens_IV.png")
 
-df2 = pd.read_excel("C:\\Users\\laris\\Desktop\\estudos_ferias20232\\python\\SW - Episode IV.xlsx", sheet_name = "SW_EpisodeV_ptBR");
 
-summary2 = df2.dropna(subset=['personagem'], axis=0)['personagem']
+df1 = pd.read_excel("C:\\Users\\laris\\Desktop\\estudos_ferias20232\\python\\SW - Episode IV.xlsx", sheet_name = "SW_EpisodeV_ptBR");
 
-all_summary2 = "\n".join(s for s in summary)
+summary1 = df1.dropna(subset=['personagem'], axis=0)['personagem']
+
+all_summary1 = "\n".join(s for s in summary1)
+wordcloud1 = WordCloud(stopwords=stopwords, background_color = "black", width=1600, height=800).generate(all_summary1)
+# mostrar a imagem final
+fig, ax = plt.subplots(figsize=(10,6))
+ax.imshow(wordcloud1, interpolation='bilinear')
+ax.set_axis_off()
+plt.imshow(wordcloud1);
+wordcloud1.to_file("personagens_V.png")
+
+
+df2 = pd.read_excel("C:\\Users\\laris\\Desktop\\estudos_ferias20232\\python\\SW - Episode IV.xlsx", sheet_name = "SW_EpisodeVI_ptBR");
+
+summary2 = df.dropna(subset=['personagem'], axis=0)['personagem']
+
+all_summary2 = "\n".join(s for s in summary2)
+
 wordcloud2 = WordCloud(stopwords=stopwords, background_color = "black", width=1600, height=800).generate(all_summary2)
 # mostrar a imagem final
 fig, ax = plt.subplots(figsize=(10,6))
 ax.imshow(wordcloud2, interpolation='bilinear')
 ax.set_axis_off()
 plt.imshow(wordcloud2);
-wordcloud2.to_file("personagens_V.png")
+wordcloud2.to_file("personagens_VI.png")
 
-df3 = pd.read_excel("C:\\Users\\laris\\Desktop\\estudos_ferias20232\\python\\SW - Episode IV.xlsx", sheet_name = "SW_EpisodeVI_ptBR");
 
-summary3 = df3.dropna(subset=['personagem'], axis=0)['personagem']
+# Filme IV - Luke - Falas
 
-all_summary3 = "\n".join(s for s in summary)
-wordcloud3 = WordCloud(stopwords=stopwords, background_color = "black", width=1600, height=800).generate(all_summary3)
+resultLuke1 = df.loc[df['personagem'] == 'LUKE', 'diálogo']
+
+all_summaryFalasLuke1 = "\n".join(s for s in resultLuke1)
+wordcloudFalasLuke1 = WordCloud(stopwords=stopwords, background_color = "black", width=1600, height=800).generate(all_summaryFalasLuke1)
 # mostrar a imagem final
 fig, ax = plt.subplots(figsize=(10,6))
-ax.imshow(wordcloud3, interpolation='bilinear')
+ax.imshow(wordcloudFalasLuke1, interpolation='bilinear')
 ax.set_axis_off()
-plt.imshow(wordcloud3);
-wordcloud3.to_file("personagens_VI.png")
+plt.imshow(wordcloudFalasLuke1);
+wordcloudFalasLuke1.to_file("falasLukeEpisodeIV.png")
+
+
+#Filme V - Falas Luke
+resultLuke2 = df1.loc[df1['personagem'] == 'LUKE', 'diálogo']
+
+all_summaryFalasLuke2 = "\n".join(s for s in resultLuke2)
+wordcloudFalasLuke2 = WordCloud(stopwords=stopwords, background_color = "black", width=1600, height=800).generate(all_summaryFalasLuke2)
+# mostrar a imagem final
+fig, ax = plt.subplots(figsize=(10,6))
+ax.imshow(wordcloudFalasLuke2, interpolation='bilinear')
+ax.set_axis_off()
+plt.imshow(wordcloudFalasLuke2);
+wordcloudFalasLuke2.to_file("falasLukeEpisodeV.png")
+
+
+#Filme VI - Falas Luke
+resultLuke3 = df2.loc[df2['personagem'] == 'LUKE', 'diálogo']
+
+all_summaryFalasLuke3 = "\n".join(s for s in resultLuke3)
+wordcloudFalasLuke3 = WordCloud(stopwords=stopwords, background_color = "black", width=1600, height=800).generate(all_summaryFalasLuke3)
+# mostrar a imagem final
+fig, ax = plt.subplots(figsize=(10,6))
+ax.imshow(wordcloudFalasLuke3, interpolation='bilinear')
+ax.set_axis_off()
+plt.imshow(wordcloudFalasLuke3);
+wordcloudFalasLuke3.to_file("falasLukeEpisodeVI.png")
